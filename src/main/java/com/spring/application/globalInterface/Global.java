d36044876970c7ed502557fdfb4842da9e9153d9
Philip <philip.strobach@gmail.com>
2020-07-26 22:38:44 +0200

    extend JPA Repository

--- src/main/java/com/spring/application/globalInterface/Global.java
+++ src/main/java/com/spring/application/globalInterface/Global.java
@@ -1,13 +1,14 @@
 package com.spring.application.globalInterface;
 
-public interface Global <T>{
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Global <T> extends JpaRepository<T, Integer>{
 	
 	public void create();
 	
 	public void update(T t);
 	
 	public void deleteAll();
-	public void deleteById(T t);
 	
 	public void show();
 	
